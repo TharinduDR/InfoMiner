91c347725be74a0e86ffd4393114cfe556e3c9a6
TharinduDR <rhtdranasinghe@gmail.com>
2021-02-25 12:51:38 +0000

    001: First Commit

--- examples/task1a/evaluation.py
+++ examples/task1a/evaluation.py
@@ -8,12 +8,16 @@ pos_label = 'ADE'
 
 
 def f1(y_true, y_pred):
+    y_true = list(map(int, y_true))
+    y_pred = list(map(int, y_pred))
     y_true = decode(y_true)
     y_pred = decode(y_pred)
     return f1_score(y_true, y_pred, labels=labels, pos_label=pos_label)
 
 
 def recall(y_true, y_pred):
+    y_true = list(map(int, y_true))
+    y_pred = list(map(int, y_pred))
     y_true = decode(y_true)
     y_pred = decode(y_pred)
     return recall_score(y_true, y_pred, labels=labels, pos_label=pos_label)
