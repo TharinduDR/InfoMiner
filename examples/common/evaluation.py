f352bf3b285c0163f73513e9f9ba069de9d5d692
TharinduDR <rhtdranasinghe@gmail.com>
2021-03-11 16:55:02 +0000

    001: First Commit

--- examples/common/evaluation.py
+++ examples/common/evaluation.py
@@ -7,22 +7,16 @@ from examples.common.converter import decode
 def f1(y_true, y_pred):
     y_true = list(map(int, y_true))
     y_pred = list(map(int, y_pred))
-    y_true = decode(y_true)
-    y_pred = decode(y_pred)
     return f1_score(y_true, y_pred)
 
 
 def recall(y_true, y_pred):
     y_true = list(map(int, y_true))
     y_pred = list(map(int, y_pred))
-    y_true = decode(y_true)
-    y_pred = decode(y_pred)
     return recall_score(y_true, y_pred)
 
 
 def precision(y_true, y_pred):
     y_true = list(map(int, y_true))
     y_pred = list(map(int, y_pred))
-    y_true = decode(y_true)
-    y_pred = decode(y_pred)
     return precision_score(y_true, y_pred)
