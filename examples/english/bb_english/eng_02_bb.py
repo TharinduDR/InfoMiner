c24106be2273c9c8e3688eb67a40d445459341d7
Lasitha <lasitha.13@itfac.mrt.ac.lk>
2021-03-30 17:43:54 +0200

    IM:NLP4IF2021:IM:NLP4IF2021:English bert-base all labels additional preprocessing step for 'nan' values

--- examples/english/bb_english/eng_02_bb.py
+++ examples/english/bb_english/eng_02_bb.py
@@ -16,7 +16,8 @@ from infominer.classification import ClassificationModel
 if not os.path.exists(TEMP_DIRECTORY): os.makedirs(TEMP_DIRECTORY)
 
 full = pd.read_csv(os.path.join("examples", "english", "data", "covid19_disinfo_binary_english_train.tsv"), sep='\t')
-full = full[full["q2_label"] != 'nan']
+# full = full[full["q2_label"] != 'nan']
+full.dropna(subset=['q2_label'])
 full['labels'] = encode(full["q2_label"])
 full = full[['tweet_text', 'labels']]
 full = full.rename(columns={'tweet_text': 'text'})
