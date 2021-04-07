3817620474d3f2ceed12308c217144e4d7795a56
Lasitha <lasitha.13@itfac.mrt.ac.lk>
2021-04-07 13:24:44 +0200

    IM:NLP4IF2021:Bulgarian using multilingual bert size counter value adjust

--- examples/sample_size_counter.py
+++ examples/sample_size_counter.py
@@ -1,8 +1,8 @@
 def sample_size_counter(count_n, count_y):
 
     if count_n > count_y:
-        return count_n // count_y if count_n // count_y <= 3 else 3
+        return count_n // count_y if count_n // count_y <= 2 else 2
 
     else:
-        return -(count_y // count_n if count_y // count_n <= 3 else 3)
+        return -(count_y // count_n if count_y // count_n <= 2 else 2)
 
